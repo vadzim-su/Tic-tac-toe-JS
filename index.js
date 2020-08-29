60c7df56d567d5d26706498b961f3b43e14dabe5
vadzim-su <vadimmm0991@gmail.com>
2020-08-30 00:42:07 +0300

    feat: fix some bugs

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 const cells = document.querySelectorAll(".game__cell");
 const cellWithX = '<img src="./xmark.png" alt="">';
 const cellWithO = '<img src="./omark.png" alt="">';
-let counterWinner = 0;
 const gameField = document.querySelector(".game__field");
-let endGame = false;
-let winner;
 const resetButton = document.querySelector("#reset");
-
 const winX = document.querySelector("#winX");
 const win0 = document.querySelector("#win0");
 const draw = document.querySelector("#draw");
-
+let counterWinner = 0;
+let endGame = false;
+let win = false;
+let winner = false;
+let newGame = false;
 let winXCounter = 0;
 let win0Counter = 0;
 let drawCounter = 0;
 
 cells.forEach((cell) => {
-  if (counterWinner < 9 && endGame == false) {
+  if (!endGame) {
     cell.addEventListener("click", drawStep);
   } else {
     cell.removeEventListener("click", drawStep);
-    endGame = false;
   }
 });
 
-gameField.addEventListener("click", findWinner);
-
 function drawStep() {
-  if (!this.innerHTML) {
+  if (!this.innerHTML && !endGame) {
     if (counterWinner % 2) {
       this.innerHTML = cellWithO;
     } else {
       this.innerHTML = cellWithX;
     }
     ++counterWinner;
+    findWinner();
   }
 }
 
 function findWinner() {
-  endGame = false;
-  const win =
+  win =
     (cells[0].innerHTML === cells[1].innerHTML &&
       cells[0].innerHTML === cells[2].innerHTML &&
       cells[0].innerHTML !== "") ||
@@ -65,26 +62,30 @@ function findWinner() {
       cells[2].innerHTML === cells[6].innerHTML &&
       cells[2].innerHTML !== "");
 
-  showWinner(win);
+  if (win) {
+    showWinner();
+  } else if (counterWinner === 9 && !win) {
+    showDraw();
+  }
+
+  win = false;
+  endGame = false;
 }
 
-function showWinner(win) {
-  if (win) {
-    endGame = true;
-    winner = `The user drawn ${counterWinner % 2 ? "X" : "O"} is winner`;
-    counterWinner % 2
-      ? (winX.innerHTML = ++winXCounter)
-      : (win0.innerHTML = ++win0Counter);
-    alert(winner);
-    resetField();
-    winner = "";
-  }
+function showWinner() {
+  winner = `The user drawn ${counterWinner % 2 ? "X" : "O"} is winner`;
+  counterWinner % 2
+    ? (winX.innerHTML = ++winXCounter)
+    : (win0.innerHTML = ++win0Counter);
+  alert(winner);
+  resetField();
+}
 
-  if (counterWinner === 9 && endGame == false) {
-    draw.innerHTML = ++drawCounter;
-    alert("It's a draw");
-    resetField();
-  }
+function showDraw() {
+  draw.innerHTML = ++drawCounter;
+  alert("It's a draw");
+
+  resetField();
 }
 
 function resetField() {
@@ -94,12 +95,11 @@ function resetField() {
     cells.forEach((cell) => {
       cell.innerHTML = "";
     });
-  } else if (winner) {
-    endGame = false;
-    gameField.removeEventListener("click", findWinner);
+  } else if (win) {
     cells.forEach((cell) => {
       cell.removeEventListener("click", drawStep);
     });
+    win = false;
   }
 }
 
